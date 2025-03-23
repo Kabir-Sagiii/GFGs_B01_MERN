d97c5ef576d43efe9849bfd718cbd0e637fc3a9f
Kabir <sagithedeveloper@gmail.com>
2025-03-23 13:45:51 +0530

    added the code

---
+++ S12_23-03-2025/react-app/firstapp/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <h2 className="app">App Component</h2>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
